21aa218cfe949aa00d982f0713635482a3cc6362
vanessuniq <vfotso@mitre.org>
2022-01-31 16:46:42 -0500

    Github API token

--- src/main/java/com/example/springboot_learning/github/GithubClient.java
+++ src/main/java/com/example/springboot_learning/github/GithubClient.java
@@ -1,8 +1,20 @@
 package com.example.springboot_learning.github;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import com.example.springboot_learning.GithubProperties;
+
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpRequest;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.stereotype.Component;
+import org.springframework.util.Base64Utils;
+import org.springframework.util.StringUtils;
 import org.springframework.web.client.RestTemplate;
 
 @Component
@@ -12,11 +24,35 @@ public class GithubClient {
 
 	private static final String EVENT_ISSUES_URL = "https://api.github.com/repos/{owner}/{repo}/issues/events";
 
-	public GithubClient(RestTemplateBuilder builder) {
-		this.restTemplate = builder.build();
+  public GithubClient(RestTemplateBuilder builder, GithubProperties properties) {
+
+		this.restTemplate = builder
+				.additionalInterceptors(new GithubAppTokenInterceptor(properties.getToken()))
+				.build();
 	}
 
 	public ResponseEntity<RepositoryEvent[]> fetchEvents(String orgName, String repoName) {
 		return this.restTemplate.getForEntity(EVENT_ISSUES_URL, RepositoryEvent[].class, orgName, repoName);
+	}
+
+  private static class GithubAppTokenInterceptor implements ClientHttpRequestInterceptor {
+
+		private final String token;
+
+		GithubAppTokenInterceptor(String token) {
+			this.token = token;
+		}
+
+		@Override
+		public ClientHttpResponse intercept(HttpRequest httpRequest, byte[] bytes,
+				ClientHttpRequestExecution clientHttpRequestExecution) throws IOException {
+			if (StringUtils.hasText(this.token)) {
+				byte[] basicAuthValue = this.token.getBytes(StandardCharsets.UTF_8);
+				httpRequest.getHeaders().set(HttpHeaders.AUTHORIZATION,
+						"Basic " + Base64Utils.encodeToString(basicAuthValue));
+			}
+			return clientHttpRequestExecution.execute(httpRequest, bytes);
+		}
+
 	}
 }
